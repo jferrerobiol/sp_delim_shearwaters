f9e8c573a98166ce16bce9e4900b845b4eb6e27e
Joan Ferrer Obiol <30528650+jferrerobiol@users.noreply.github.com>
2022-11-28 09:25:01 +0100

    Delete PCA_DAPC directory

--- PCA_DAPC/Delete_Outgroups_From_VCF.sh
+++
@@ -1,6 +0,0 @@
-#Delete outgroups to carry out PCA and DAPC analysis
-vcftools --remove-indv YOUR_INDIVIDUALS_NAME --vcf your_snps.vcf --out your_filtered_snps.vcf
-
-#Get unlinked SNPs
-cat Puffinus_clust89_percent95.vcf | awk '!seen[$1]++' > Puffinus_clust89_percent95_unlinked_snps.vcf
-
